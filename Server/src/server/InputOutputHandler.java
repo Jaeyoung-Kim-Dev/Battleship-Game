442ca169e02b3e94fce2d39985004a41faff65e8
Jaeyoung Kim <jaeyoung.kim.dev@gmail.com>
2020-10-19 00:18:55 -0600

    Export Jar files

---
+++ Server/src/server/InputOutputHandler.java
@@ -0,0 +1,83 @@
+/**
+ * 
+ */
+package server;
+
+import java.io.IOException;
+
+import problemdomain.*;
+
+/**
+ * Handler serialized data from the client
+ * 
+ * @author Jaeyoung Kim
+ *
+ */
+public class InputOutputHandler implements Runnable {
+	private ClientConnection input;
+	private ClientConnection output;
+	private ServerGUI serverGUI;
+
+	/**
+	 * User-defined constructor for InputOutputHandler.
+	 * 
+	 * @param input a user ClientConnection
+	 * @param output enemy ClientConnection
+	 * @param serverGUI GUI for server
+	 */
+	public InputOutputHandler(ClientConnection input, ClientConnection output, ServerGUI serverGUI) {
+		this.input = input;
+		this.output = output;
+		this.serverGUI = serverGUI;
+	}
+
+	/**
+	 * run method
+	 */
+	@Override
+	public void run() {
+		while (!this.input.getSocket().isClosed() && !this.output.getSocket().isClosed()) {
+			try {
+				Object receive = (Object) this.input.getOis().readObject();
+				if (receive instanceof StartGame) {
+					StartGame startGame = (StartGame) receive;
+					serverGUI.addMessage(startGame.toString());
+					this.output.getOos().writeObject(startGame);
+				} else if (receive instanceof Message) {
+					Message message = (Message) receive;
+					serverGUI.addMessage(message.toString());
+					this.output.getOos().writeObject(message);
+				} else if (receive instanceof Aim) {
+					Aim aim = (Aim) receive;					
+					this.output.getOos().writeObject(aim);
+				}else if (receive instanceof Battle) {
+					Battle battle = (Battle) receive;
+					serverGUI.addMessage(battle.toString());
+					this.output.getOos().writeObject(battle);
+				} else if (receive instanceof AfterAttack) {
+					AfterAttack afterAttack = (AfterAttack) receive;
+					serverGUI.addMessage(afterAttack.toString());
+					this.output.getOos().writeObject(afterAttack);
+				} else if (receive instanceof ReGame) {
+					ReGame reGame = (ReGame) receive;
+					serverGUI.addMessage(reGame.toString());
+					this.input.getSocket().close();
+					this.output.getOos().writeObject(reGame);
+					//this.output.getSocket().close();			
+				} else if (receive instanceof QuitGame) {
+					QuitGame quitGame = (QuitGame) receive;
+					serverGUI.addMessage(quitGame.toString());					
+					this.input.getSocket().close();	
+					this.output.getOos().writeObject(quitGame);
+					//this.output.getSocket().close();
+					//소켓을 여기서 닫아야 while문을 빠져나와서 EOF예외처리에서 빠져나올수 있는것인가...?
+				}
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();				
+			} catch (IOException e) {
+				e.printStackTrace();		
+			}
+		}
+	}
+
+}
---
+++ Server/src/server/ServerDriver.java
@@ -0,0 +1,70 @@
+package server;
+
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import problemdomain.*;
+
+/**
+ * Includes main method for Battleship game server.
+ * 
+ * @author Jaeyoung Kim
+ * @version 1.0, October 7, 2020
+ *
+ */
+public class ServerDriver  {
+
+	public static void main(String[] args) throws IOException {		
+		ServerGUI serverGUI = new ServerGUI();
+		serverGUI.display();
+		
+		ServerSocket listener = new ServerSocket(1234);
+
+		serverGUI.addMessage("Listening on port 1234.");
+		serverGUI.addMessage("Waiting for connection...");
+		
+		ArrayList<ClientConnection> connections = new ArrayList<>();
+		
+		while (listener.isBound()) {
+			try {
+				Socket client = listener.accept();
+
+				serverGUI.addMessage("Client connected.");
+
+				InputStream inputStream = client.getInputStream();
+				ObjectInputStream objectInputStream = new ObjectInputStream(inputStream);
+
+				OutputStream outputStream = client.getOutputStream();
+				ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream);
+
+				ClientConnection connection = new ClientConnection(client, objectInputStream, objectOutputStream);
+
+				connections.add(connection);
+
+				if (connections.size() % 2 == 0) {
+					ClientConnection connection1 = connections.get(0);
+					ClientConnection connection2 = connections.get(1);
+
+					// Spin up a thread to handle connections
+					ClientHandler clientHandler = new ClientHandler(connection1, connection2, serverGUI);
+					Thread thread = new Thread(clientHandler);
+					
+					thread.start();
+					
+					// Remove connections array list
+					connections.remove(connection1);
+					connections.remove(connection2);
+				}
+
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+		}
+
+		listener.close();			
+	}
+
+}
